44047c01ac4cd44eaca45226906a7b60e43bb4eb
Paul Stewart <paul@onefastsnail.com>
2017-11-21 22:50:12 +0200

    Added quick search and display of all folders in versions so we can browse

--- dist/index.php
+++ dist/index.php
@@ -1,1 +1,9 @@
-<?php echo phpinfo();
+<?php 
+
+$dirs = glob('../versions/*' , GLOB_ONLYDIR);
+
+foreach($dirs as $dir){
+    printf('<p><a href="//%s.paul.em87.io" target="_blank">%s.paul.em87.io</a></p>', basename($dir), basename($dir));
+}
+
+echo phpinfo();
